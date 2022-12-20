5b8b528cc48301583aa5131fce5d7f741a89e61a
amarmahure <121081989+amarmahure@users.noreply.github.com>
2022-12-20 12:05:46 +0530

    Update spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\n WELCOME");
+  printf("\n AMAR ");
+  add();
+}
+void add()
+{
+  int a,b,c;
+  printf("ENter no:");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf(" \n add=%d",c);
 }
